6a8b95bf83296a724e9bbc443ac214e481978f8f
Поклад Алексей Юрьевич <aliaksei.poklad@softclub.by>
2024-01-25 16:44:15 +0300

    feat: implement hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from "crypto";
+import { createReadStream } from "node:fs";
+import * as tools from "../tools.js";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileToCalculate = tools.setFilePath(
+    import.meta.url,
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+
+  const hash = createHash("sha256");
+
+  const readStream = createReadStream(fileToCalculate, { encoding: "utf8" });
+
+  readStream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
 };
 
 await calculateHash();
