a15cf9abfed5ab099349b74d6a0b757292b0cf38
Поклад Алексей Юрьевич <aliaksei.poklad@softclub.by>
2024-01-26 14:38:21 +0300

    feat: implement zip module

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { promisify } from "util";
+import { pipeline } from "stream";
+import * as tools from "../tools.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const pipelineAsync = promisify(pipeline);
+  const inputFile = tools.setFilePath(import.meta.url, "files", "archive.gz");
+  const outputFile = tools.setFilePath(
+    import.meta.url,
+    "files",
+    "fileToCompress.txt"
+  );
+
+  await pipelineAsync(
+    createReadStream(inputFile),
+    createGunzip(),
+    createWriteStream(outputFile)
+  );
 };
 
 await decompress();
